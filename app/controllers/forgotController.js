7c1ab5eb975cc0e3c6bebd14240d3f88e19fe7e7
x143678 <leonardo.souza@prservicos.com.br>
2020-06-12 23:54:32 -0300

    Reset enviando email (via api)

---
+++ app/controllers/forgotController.js
@@ -0,0 +1,87 @@
+// const {
+//     Usuario
+// } = require('../models')
+// const config = require("../configs/database");
+// const bcrypt = require("bcrypt");
+// const Email = require('../configs/email');
+// const fetch = require('node-fetch');
+// const API_BASE = 'http://localhost:3000/api/v0'
+require('dotenv').config();
+const db = require('../models');
+const crypto = require('crypto');
+const Email = require('../config/email');
+const async = require('async');
+
+const forgotController = function(req,res,next) {
+    async.waterfall([   
+        function(done) {
+            console.log('step 1')
+            crypto.randomBytes(10, function(err, buf) {
+              var token = buf.toString('hex');
+              done(err, token);
+            });
+
+          },
+          function(token, done) {
+              console.log('step2',token)
+              db.User.findOne(
+                { where: {email: req.body.email}}).then(function(user){
+                    if(!user){
+                        req.flash('error', 'No account with that email address exists.');
+                        return res.status(201).send('Email não encontrado, segue a vida',token);
+                    } else {
+                        user.resetPasswordToken = token;
+                        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+                        user.save
+                            done(null,token, user);
+                        }
+                    });
+
+            // db.User.findOne({ where: {email: req.body.email}},function(err, user) {
+            // // db.User.findOne({ email: req.body.email }, function(err, user) {
+            //   if (!user) {
+            //     req.flash('error', 'No account with that email address exists.');
+            //     // return res.redirect('/forgot');
+            //     return res.send('No account with that email address exists.')
+            //   }
+      
+            //   db.User.resetPasswordToken = token;
+            //   db.User.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+      
+            //   db.User.save(function(err) {
+            //     done(err, token, user);
+            //   });
+            // });
+          },
+          function(token, user, done) {
+            console.log('Envio Email')
+            let mailOptions = {
+              to: user.email,
+              from:`${process.env.EMAIL_USER}`,
+              subject: 'CRUD candySphere Password Reset',
+              text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+            };
+            Email.sendMail(mailOptions, function(err) {
+              req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+              done(err, 'done');
+            });
+          }
+        ], function(err) {
+          if (err) return next(err);
+          res.redirect('/resetPassword');
+        });
+};
+
+
+// const forgotController = async function(req,res,next){
+//     const token = '1a2b3c';
+
+
+
+// }
+
+
+module.exports = forgotController
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,9 @@
 // Requiring our models and passport as we've configured it
 const db = require('../app/models');
 const passport = require("../app/config/passport");
+const forgotController = require('../app/controllers/forgotController');
+const Email = require('../app/config/email');
+
 //
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
@@ -66,6 +69,68 @@ module.exports = function(app) {
       })
   })
 
+  app.post('/api/forgot', forgotController);
+  // app.post('/api/forgot', function(req,res){
+  //   return res.status(200).json(req.body.email);
+  // });
+
+  // app.post('/api/forgot', function(req, res, next) {
+    
+  //   async.waterfall([
+  //     function(done) {
+  //       crypto.randomBytes(20, function(err, buf) {
+  //         var token = buf.toString('hex');
+  //         done(err, token);
+  //       });
+  //     },
+  //     function(token, done) {
+  //       User.findOne({ email: req.body.email }, function(err, user) {
+  //         if (!user) {
+  //         //   console.log('error', 'No account with that email address exists.');
+  //         req.flash('error', 'No account with that email address exists.');
+  //           return res.redirect('/forgot');
+  //         }
+  // console.log('step 1')
+  //         user.resetPasswordToken = token;
+  //         user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+  
+  //         user.save(function(err) {
+  //           done(err, token, user);
+  //         });
+  //       });
+  //     },
+  //     function(token, user, done) {
+  //       console.log('step 2')
+  //       var mailOptions = {
+  
+  //         to: user.email,
+  //         from: 'myemail',
+  //         subject: 'Node.js Password Reset',
+  //         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+  //           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+  //           'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+  //           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+  
+  //       };
+  //       console.log('step 3')
+  
+  //         Email.sendMail(mailOptions, function(err) {
+  //         req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+  //         console.log('sent')
+  //         res.redirect('/forgot');
+  // });
+  // }
+  //   ], function(err) {
+  //     console.log('this err' + ' ' + err)
+  //     res.redirect('/');
+  //   });
+  // });
+  
+  // app.get('/forgot', function(req, res) {
+  //   res.render('forgot', {
+  //     User: req.user
+  //   });
+  // });
 
 };
 
